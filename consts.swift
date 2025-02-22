0958a62ece3f1ffb90b09d39bc1fed44ebf972c1
meowster711 <163609991+meowster711@users.noreply.github.com>
2025-02-22 13:12:57 -0800

    Update consts.swift

--- consts.swift
+++ consts.swift
@@ -11,6 +11,22 @@ import Foundation
 // convert miles to meters
 let  mDist =  1.0 * 1609.34
 
+// Location struct
+struct bLocation: Codable {
+    let id: Int
+    let name: String
+    let latitude: Double
+    let longitude: Double
+    let notes : String
+}
+
+
 //functionally null bloc
 let null_bLoc = bLocation(id: 0, name: "", latitude: 0, longitude: 0, notes: "")
 
+// state that when changed updates view, global
+class AppState: ObservableObject {
+    static let shared = AppState() // Singleton instance
+
+    @Published var selectedDestination: bLocation? = nil
+}
