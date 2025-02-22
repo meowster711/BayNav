5cf0e6408f8f241da4c64c312550edabcea109eb
meowster711 <163609991+meowster711@users.noreply.github.com>
2025-02-22 13:10:44 -0800

    Update Home View.swift

--- Home View.swift
+++ Home View.swift
@@ -6,30 +6,61 @@
 //
 
 import SwiftUI
-import SwiftData
 import MapKit
 
 struct HomeView: View {
-    @Environment(\.modelContext) private var modelContext
-    
-    
+    @EnvironmentObject var locationManager: LocationManager
+    @EnvironmentObject var appState: AppState
+
     var body: some View {
         NavigationStack {
-            Text ("Map") //TODO: use MKMap
-                .navigationTitle("BayNav")
-                .toolbar { // TODO : jaden pls make look pretty
-                    ToolbarItemGroup(placement: .bottomBar) { NavigationLink(destination: LocationsView())
-                        {Image(systemName: "flag").foregroundColor(.black) }
-                        NavigationLink(destination: SettingsView())
-                            { Image(systemName: "gear").foregroundColor(.black) }
+            // Main content
+            if let userLocation = locationManager.userLocation {
+                MapView(userLocation: .constant(userLocation.coordinate))
+                    .edgesIgnoringSafeArea(.all)
+            } else {
+                Text("Error in Locating...")
+            }
+        }
+        // JADEN TO DO
+        .navigationTitle("BayNav") 
+        .toolbar {
+            ToolbarItemGroup(placement: .bottomBar) {
+                NavigationLink(destination: LocationsView()) {
+                    Image(systemName: "flag")
+                        .foregroundColor(.black)
+                }
+                NavigationLink(destination: SettingsView()) {
+                    Image(systemName: "gear")
+                        .foregroundColor(.black)
+                }
+            }
+        }
+        //END JADEN TO DO
+        .navigationDestination(
+            isPresented: Binding<Bool>(
+                get: { appState.selectedDestination != nil },
+                set: { newValue in
+                    if !newValue {
+                        appState.selectedDestination = nil // Reset the state
                     }
                 }
-        }
+            ),
+            destination: {
+                if let destination = appState.selectedDestination {
+                    NavView(dest: destination)
+                }
+            }
+        )
     }
 }
 
 
-
 #Preview {
-    HomeView()
+    NavigationStack {
+           HomeView()
+               .environmentObject(MockLocationManager() as LocationManager)
+               .environmentObject(AppState()) 
+
+       }
 }
