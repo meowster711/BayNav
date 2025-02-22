39eeed5d3b57d5681859a2518f851df48576e2ca
meowster711 <163609991+meowster711@users.noreply.github.com>
2025-02-22 13:11:14 -0800

    Update Location Handling.swift

--- Location Handling.swift
+++ Location Handling.swift
@@ -20,32 +20,45 @@ class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
     override init() {
         super.init()
         locationManager.delegate = self
-        locationManager.requestAlwaysAuthorization()
+        locationManager.requestAlwaysAuthorization() // request "always" location
         locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
         locationManager.distanceFilter = 10
         locationManager.activityType = .automotiveNavigation
         locationManager.startUpdatingLocation()
+        locationManager.allowsBackgroundLocationUpdates = true // Allow background updates
     }
 
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        guard let location = locations.last else { return }
-        userLocation = location
-    }
 
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
         print("Location manager failed with error: \(error.localizedDescription)")
     }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        // triggers whenever location is updated (every 1 sec)
+        guard let userLocation = locations.last else { return }
+        self.userLocation = userLocation
+
+        //  takes user location and returns closest bay <= mDist mi else return bLocation with id 0
+        // this is O(n) linear search that terminates when found. O(n) per 1 sec as default "always" location turned on
+            // further implementation: 2mi x 2mi grid in which long & lat are keys to hash table. entries are locations, and then linear search that subset to see if we are w/i 1mi of each location
+            // monitor if user leaves 2mix2mi block, so will be O( 1 * subset size) . faster but more overhead .
+        
+        let dest = null_bLoc // default if no close loc
+        for  bay in bays{
+            if CLLocation(latitude: bay.latitude, longitude: bay.longitude).distance(from: userLocation) <= mDist {let dest = bay}
+        }
+
+        if dest.id != 0 {
+            triggerNotification(dest: dest)
+        }
+    }
 }
 
-class LocationUtils {
-    func findClosestBay (userLocation:CLLocation ) -> bLocation{ // takes user location and returns closest bay <= mDist mi else return bLocation with id 0
-        // this is O(n) linear search that terminates when found... could be better
-        for  bay in bays{
-            var bayLocation = CLLocation(latitude: bay.latitude, longitude: bay.longitude)
-            if bayLocation.distance(from: userLocation) <= mDist {return bay}
-        }
-        return null_bLoc
+class MockLocationManager: LocationManager {
+    override init() {
+        super.init()
+        // Set a default location for previews so that there is no crash for debugging
+        self.userLocation = CLLocation(latitude: 34.11136, longitude: -118.02725)
     }
-    
 }
 
